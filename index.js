f192abff9843cc4a8b19605d3bff8e8e4cac721f
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-18 13:05:02 -0700

    Understand how functions determine scope in JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var customerName = "customerOne";
+function uppercaseVariable(){
+  console.log (customerName.toUpperCase())
+} 
+function setBestCustomer() {
+  bestCustomer = "Not Bob";
+  
+} 
 // Write your solution in this file!
